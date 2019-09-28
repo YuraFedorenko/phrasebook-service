2a776994c10c74f70d53f9223ccd8d4b9ab1702a
YuraFedorenko <yurafedorenko721@gmail.com>
2019-09-28 10:31:00 +0300

    config client added

--- src/main/java/com/lollion/phrasebookservice/controller/PhraseController.java
+++ src/main/java/com/lollion/phrasebookservice/controller/PhraseController.java
@@ -2,6 +2,8 @@ package com.lollion.phrasebookservice.controller;
 
 import com.lollion.phrasebookservice.service.PhrasebookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,21 @@ public class PhraseController {
 
     private final PhrasebookService phrasebookService;
 
+    @Value("${config.phrase}")
+    private String configPhrase;
+
     @GetMapping("/translation")
     public String translate(@NotNull @RequestParam(name = "phrase") String phrase,
                                    @NotNull @RequestParam(name = "from") String from,
                                    @NotNull @RequestParam(name = "to") String to) {
         return phrasebookService.translate(phrase, from, to);
+    }
+
+    @RefreshScope
+    @GetMapping("/config-translation")
+    public String translateFromConfig(@NotNull @RequestParam(name = "from") String from,
+                            @NotNull @RequestParam(name = "to") String to) {
+        return phrasebookService.translate(configPhrase, from, to);
     }
 
     @DeleteMapping("/translation")
